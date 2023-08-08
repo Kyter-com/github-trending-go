97b2eef47c103eb8e3300735f1faf5dac8da9c63
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-08-07 19:42:07 -0700

    feat: setup basic request

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("🏁 Starting Bot")
+
+	// Load our .env file
+	godotenv.Load()
+
+	req, err := http.NewRequest(http.MethodGet, os.Getenv("CLOUDFLARE_KV_URL")+"/values/go", nil)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("CLOUDFLARE_KV_KEY")))
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("res status code", res.StatusCode)
 }
